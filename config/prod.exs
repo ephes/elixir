d11e0efaacb0cd70160792a84cb7140f95797c50
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-11-15 17:45:35 +0100

    bind only to localhost

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :hello, HelloWeb.Endpoint,
-  url: [host: "phoenix.django-cast.com", port: 8002],
+  url: [host: "phoenix.django-cast.com", port: 8002, ip: {127,0,0,1}],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
